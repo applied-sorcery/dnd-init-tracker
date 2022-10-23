738c3ca57b35aa92638e26d21b8a87d4d27c430f
tim <tim.auldridge@outlook.com>
2022-10-23 14:58:30 -0700

    hide resume button if no fighters in list

--- src/components/CombatMenu.js
+++ src/components/CombatMenu.js
@@ -12,6 +12,7 @@ const CombatMenu = ({
   onNewCombatPress,
   onLoadCombatPress,
   setCurrentView,
+  fighters,
 }) => (
   <View style={Styles.combatMenu}>
     <View style={Styles.someButtonsColumn}>
@@ -27,14 +28,18 @@ const CombatMenu = ({
       >
         <Text style={Styles.defaultText}>LOAD COMBAT</Text>
       </TouchableOpacity>
-      <TouchableOpacity
-        style={[Styles.controlButton, { minWidth: "60%", maxHeight: "20%" }]}
-        onPress={() => {
-          setCurrentView("PreBattle");
-        }}
-      >
-        <Text style={Styles.defaultText}>RESUME</Text>
-      </TouchableOpacity>
+      {fighters?.length ? (
+        <TouchableOpacity
+          style={[Styles.controlButton, { minWidth: "60%", maxHeight: "20%" }]}
+          onPress={() => {
+            setCurrentView("PreBattle");
+          }}
+        >
+          <Text style={Styles.defaultText}>RESUME</Text>
+        </TouchableOpacity>
+      ) : (
+        ""
+      )}
     </View>
     <Text style={Styles.defaultText}>Recent Combats:</Text>
   </View>
